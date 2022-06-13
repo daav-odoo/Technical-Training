2f4d2f912ca637c99d728c37323541bce095b5db
daav-odoo <odoo@ampd1.odoo.com>
2022-06-13 16:00:32 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,37 @@
+{
+
+'name': 'Odoo Academy'
+
+'summary': """Academy app to manage Training"""
+
+'Description': """
+
+    Academy Module to manage Training:
+
+        - Courses
+
+        - Sessions
+
+        - Attendees
+
+""",
+
+'author': 'daav-odoo'
+
+'website': 'https://www.odoo.com'
+
+'category': 'Training',
+
+'version': '0.1'
+
+'depends': ['base'],
+
+'data': [
+
+],
+
+'demo': [
+
+],
+
+}
